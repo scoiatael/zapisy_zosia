990bd42d22f38b16c4cefb8721c2a021e4a4679b
root <root@chaosforge.net>
2013-01-14 23:35:32 +0100

    przygotowanie otwarcia

--- urls.py
+++ urls.py
@@ -24,15 +24,15 @@ urlpatterns = patterns('',
 
      (r'^$', blog.views.index),
 
-     (r'^rooms/$', newrooms.views.index),
-     (r'^rooms/list.json$', newrooms.views.json_rooms_list),
+     #(r'^rooms/$', newrooms.views.index),
+     #(r'^rooms/list.json$', newrooms.views.json_rooms_list),
 
      #(r'^rooms/fill/$', newrooms.views.fill_rooms),
-     (r'^rooms/modify/$', newrooms.views.modify_room), 
-     (r'^rooms/open/$', newrooms.views.open_room), 
-     (r'^rooms/close/$', newrooms.views.close_room), 
-     (r'^rooms/trytogetin/$', newrooms.views.trytogetin_room), 
-     (r'^leave_room/$', newrooms.views.leave_room), 
+     #(r'^rooms/modify/$', newrooms.views.modify_room), 
+     #(r'^rooms/open/$', newrooms.views.open_room), 
+     #(r'^rooms/close/$', newrooms.views.close_room), 
+     #(r'^rooms/trytogetin/$', newrooms.views.trytogetin_room), 
+     #(r'^leave_room/$', newrooms.views.leave_room), 
 
      (r'^blog/$', blog.views.index),
 
